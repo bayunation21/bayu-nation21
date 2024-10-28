ae8857dca6f04776befcdc1cf7c97553638c5974
bayunation21 <mahabayu10@gmail.com>
2024-10-29 00:34:25 +0700

    Add files via upload

---
+++ program bayu/main.js
@@ -0,0 +1,5 @@
+// Misalnya, menambahkan fungsi sederhana untuk alert di tombol Get Started
+document.querySelector(".btn-success").addEventListener("click", function() {
+    alert("Selamat datang di aplikasi Gojek!");
+  });
+  
